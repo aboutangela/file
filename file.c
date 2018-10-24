3cf51b4778049bfee6fdce0b0aa02562cdeaa7ca
Kathleen Wong <kwong17@stuy.edu>
2018-10-23 21:11:37 -0400

    .

--- file.c
+++ file.c
@@ -4,21 +4,26 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
+
 
 int main(){
-  int test = open("text.txt", O_RDWR);
+  int test = open("text.txt", O_RDWR | O_APPEND);
   printf("open result: %d \n", test);
   int test2 = open("test.txt", O_RDWR);
   printf("open error: %d \n", test2);
 
-  char stored[6];
+  char *stored= malloc(20);
   int rtest = read(test, stored, 6);
   printf("read result: %s \n", stored);
 
-  int wtest = write(test, "result", 6);
-  //char wstored[12];
-  //int ntest = read(test, wstored, 12);
-  //printf("read after write result: %s \n", wstored);
+  char *stored2 = malloc(20);
+  char *stored3 = malloc(20);
+  strcat(stored2,"skeleton");
+  int wtest = write(test, stored2, strlen(stored2));
+  int test3 = open("text.txt", O_RDWR | O_APPEND);
+  int atest = read(test3, stored2, 14);
+  printf("read result after write: %s \n", stored2);
 
   int ctest = close(test);
   printf("close result: %d \n", ctest);
